e21b007be2ff3b0342053067e4f3be35233f46b1
leeo2 <leeo@hartwick.edu>
2019-11-13 20:12:23 -0500

    Edited

--- person.py
+++ person.py
@@ -1,1 +1,18 @@
 # Olivia Lee
+# 11-13-19
+# Person class
+
+class Person():
+
+    def __init__(self, first, last, age, hair_color, eye_color):
+        self.first = first
+        self.last = last
+        self.age = age
+        self.hair_color = hair_color
+        self.eye_color = eye_color
+
+    def description(self, first, last, age, hair_color, eye_color):
+        return f"{first} {last} is " + str(self.age) + f"years old. Their hair is {self.hair_color} and their eyes {self.eye_color}."
+
+    def birthday(self, age):
+        self.age = self.age + 1
